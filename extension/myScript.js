769d00cd27c506268ac0fea7bcb890275f6c6b61
Tong Hui Kang <tonghuikang@outlook.com>
2018-09-02 00:08:34 +0800

    commented on the code relevant to our project idea

--- extension/myScript.js
+++ extension/myScript.js
@@ -5,6 +5,7 @@
  * @author Mark Craft, Qinglin Chen
  * @date Fall 2016
  */
+
 // what is this part for?
 (function(document) {
 'use strict';
@@ -35,7 +36,9 @@ function httpGet(input, type, data) {
 	fetch(theUrl)
 // get the reply which is put into the variable text, which is "verified" or otherwise
 		.then(text).then(function(text) {
-// this create a button element beside the title
+// this creates a button element beside the title
+// we will use this to create a hyperlink beside the timestamp
+// we do not need to loading button
 			var btn = document.createElement('div'),
 				button = Ladda.create(btn);
 			btn.style = "font-weight:bold; padding: 3px; position:absolute; top: 4px; right: 30px;background: #3b5998; font-size: 15px;";
@@ -59,6 +62,7 @@ function httpGet(input, type, data) {
  * @param whether the server is down or not
  */
 // this function is not mentioned anywhere else in the repo
+// ignore this part
 function createButton(btn, loc) {
 	var btn = document.createElement('div'),
 		button = Ladda.create(btn);
@@ -76,16 +80,16 @@ function createButton(btn, loc) {
  * @param the information to display
  */
 // mentioned only in createButton which is not mentioned anywhere else
-function hoverTooltip(info) {
-	//console.log("hovering: " + info);
-}
+// ignore this part
+// function hoverTooltip(info) {
+// 	//console.log("hovering: " + info);
+// }
 
 /*
  * Parse through Facebook's encoded url for the actual url
  *
  */
-// str is the output, var whatever is the input.
-// taking this as a black box for now.
+// not sure to what extent will this be used
 function parseUri(str) {
   var o = parseUri.options,
     m = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
@@ -136,37 +140,49 @@ function decode(code) {
 // httpGet brings posts to the backend
 // goes through the relevant elements
 setInterval(function() {
-// where does document come from?
+// I think document refers to the entire script
+// identifies the Facebook post
 	var test = document.getElementsByClassName('_4-u2 mbm _5v3q _4-u8');
 // now test is an array of elements
 	for(var i=0; i<test.length; i++) {
 // fetch one of the elements
 		var data = test[i];
 
-		// Check if feed needs to be modified
-
+		// Check if feed needs to be modified - only send if it is modified
 		if(!feeds.has(data)) {
 			feeds.add(data);
 
 			// Send server requests
-
+            
 			var statement = "";
 
 			var processed = false;
-
+            
+            // Go through every element of the post and process it
+            // For our application:
+            // Given any Facebook post - which is the var test
+            //// For original posting - the picture is uploaded by the poster
+            //// -- the post contains an image, a description by the poster
+            //// For shared posting - the picture is uploaded by someone else
+            //// -- the post contains an image, a descirption by the poster and by the sharer
+            // We will only be concerned with the description by the original poster
+            // Pipeline:
+            //// Send the picture to Vision API to extract the text.
+            //// Send the extracted text concatenated with the poster text to Natural Language API to extract keywords
+            //// Send the extracted keywords to Google custom search API for the link to fact-checking websites
+            //// Create a hyperlinked text beside the timestamp of the original post 
+            //// - (refer to above on how to create a button)
 			var linked = test[i].querySelector('._6ks');
 			if(!processed && linked != null && linked.querySelector('a') != null) {
 				processed = true;
 				httpGet(linked.querySelector('a').href, "url", data);
 			}
 
-
 			var link = test[i].querySelector('._5pbx.userContent');
 			if(!processed && link != null && link.querySelector('a') != null && link.querySelector('a').href != null) {
 				processed = true;
 				httpGet(link.href, "url", data);
 			}
-
 
 			var picComment = test[i].querySelector('.uiScaledImageContainer._4-ep');
 			if(!processed && picComment != null && picComment.querySelector('img') != undefined && picComment.querySelector('img').src != null) {
